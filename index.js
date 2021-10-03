7ed388e54c041594ba4efb5c59f742a359d352ed
monsieurpiyushsingh <piyushsinghlucky98@gmail.com>
2021-10-03 23:53:00 +0530

    Updated index

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const logger = require('./middleware/logger')
 const nodemailer = require("nodemailer");
 const app = express()
 
-const port = process.env.PORT || 3000
+const port = 80 //process.env.PORT || 3000
 
 app.use(express.urlencoded({
     extended: true
